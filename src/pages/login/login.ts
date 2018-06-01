afdf2ab286dee2c1137e2920640ae79821213adf
ddmalqui <ddmalqui@gmail.com>
2018-06-01 12:08:06 -0300

    redirect

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -28,16 +28,26 @@ export class LoginPage {
   passwordtype:string='password';
   passeye:string ='eye';
   constructor(public toastCtrl: ToastController, public fb: FormBuilder, public navCtrl: NavController,
-   public navParams: NavParams, public afAuth: AngularFireAuth,
-   public loadingProvider: LoadingProvider,
-   public facebook: Facebook) {
+    public navParams: NavParams, public afAuth: AngularFireAuth,
+    public loadingProvider: LoadingProvider,
+    public facebook: Facebook) {
   	this.authForm = this.fb.group({
       'email' : [null, Validators.compose([Validators.required])],
       'password': [null, Validators.compose([Validators.required])],
     });
 
-        this.email = this.authForm.controls['email'];
-        this.password = this.authForm.controls['password'];
+    this.email = this.authForm.controls['email'];
+    this.password = this.authForm.controls['password'];
+
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.navCtrl.setRoot('ProfilePage');
+      } else {
+     // not signed in.
+   }
+ })
+
   }
 
   ionViewDidLoad() {
@@ -49,56 +59,58 @@ export class LoginPage {
 
 // For User Login
 
-  userLogin(loginData){
-    this.loadingProvider.startLoading();
-  	console.log('loginData',loginData);
-  		this.afAuth.auth.signInWithEmailAndPassword(loginData.email, loginData.password)
-        .then(result => {
-          console.log('result >>',result);
-          this.loadingProvider.stopLoading();
-          this.moveToHome(result);
-        }).catch(err => {
-          this.loadingProvider.stopLoading();
-          console.log('err',err);
-          this.presentToast(err);
-        });
-  }
+userLogin(loginData){
+  this.loadingProvider.startLoading();
+  console.log('loginData',loginData);
+  this.afAuth.auth.signInWithEmailAndPassword(loginData.email, loginData.password)
+  .then(result => {
+    console.log('result >>',result);
+    this.loadingProvider.stopLoading();
+    this.moveToHome(result);
+  }).catch(err => {
+    this.loadingProvider.stopLoading();
+    console.log('err',err);
+    this.presentToast(err);
+  });
+}
 
 
 // For Social Login
 
-  socialLogin(isLogin){
-  	if (isLogin == "facebook"){
-      this.loadingProvider.startLoading();
+socialLogin(isLogin){
+  if (isLogin == "facebook"){
+    this.loadingProvider.startLoading();
 
-      let provider = new firebase.auth.FacebookAuthProvider();
-        firebase.auth().signInWithRedirect(provider).then(() => {
-          this.loadingProvider.startLoading();
-            firebase.auth().getRedirectResult().then((result)=>{
-              console.log('result',result);
-              this.moveToHome(result.user);
-             this.loadingProvider.stopLoading();
-            }).catch(function(error){
-              this.loadingProvider.stopLoading();
-              alert(error.message);
-              console.log('error',error);
-            })
-            this.loadingProvider.stopLoading();
-        }).catch(function(error){
-          this.loadingProvider.stopLoading();
-          alert(error.message);
-          console.log('error',error);
-        })
-        this.loadingProvider.stopLoading();
-  	}else if(isLogin == "google"){
+    let provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithRedirect(provider).then(() => {
       this.loadingProvider.startLoading();
-      let provider = new firebase.auth.GoogleAuthProvider();
-        firebase.auth().signInWithRedirect(provider).then(() => {
-          this.loadingProvider.startLoading();
-            firebase.auth().getRedirectResult().then((result)=>{
-              console.log('result',result);
-              this.loadingProvider.stopLoading();
-              this.navCtrl.setRoot('ProfilePage');
+      firebase.auth().getRedirectResult().then((result)=>{
+        console.log('result',result);
+        this.moveToHome(result.user);
+        this.loadingProvider.stopLoading();
+      }).catch(function(error){
+        this.loadingProvider.stopLoading();
+        alert(error.message);
+        console.log('error',error);
+      })
+      this.loadingProvider.stopLoading();
+    }).catch(function(error){
+      this.loadingProvider.stopLoading();
+      alert(error.message);
+      console.log('error',error);
+    })
+    this.loadingProvider.stopLoading();
+  }else if(isLogin == "google"){
+    this.loadingProvider.startLoading();
+    let provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithRedirect(provider).then(() => {
+      this.loadingProvider.startLoading();
+      firebase.auth().getRedirectResult().then((result)=>{
+        console.log('asdf');
+        console.log(result);
+        console.log('asdf');
+        this.loadingProvider.stopLoading();
+        this.navCtrl.setRoot('ProfilePage');
               //this.moveToHome(result.user);
             }).catch(function(error){
               this.loadingProvider.stopLoading();
@@ -106,25 +118,25 @@ export class LoginPage {
               console.log('error',error);
             })
             this.loadingProvider.stopLoading();
-        }).catch(function(error){
+          }).catch(function(error){
+            this.loadingProvider.stopLoading();
+            alert(error.message);
+            console.log('error',error);
+          })
           this.loadingProvider.stopLoading();
-          alert(error.message);
-          console.log('error',error);
-        })
-        this.loadingProvider.stopLoading();
-  	}else if(isLogin == "twitter"){
+        }else if(isLogin == "twitter"){
   		// this.afAuth.auth.signInWithPopup(new firebase.auth.TwitterAuthProvider())
       // 	.then(res => {
       // 		 this.moveToHome(res);
       // 	})
       // 	.catch(err => console.log('err',err));
-  	}else if(isLogin == "github"){
+    }else if(isLogin == "github"){
   		// this.afAuth.auth.signInWithPopup(new firebase.auth.GithubAuthProvider())
       // 	.then(res => {
       // 		 this.moveToHome(res);
       // 	})
       // 	.catch(err => console.log('err',err));
-  	}
+    }
 
   }
 
@@ -136,33 +148,39 @@ export class LoginPage {
   //Move to Home Page
   moveToHome(res){
   	console.log('res',res);
-  	this.navCtrl.setRoot('ProfilePage',{res:res});
+    //this.navCtrl.setRoot('ProfilePage',{res:res});
+    this.navCtrl.setRoot('ProfilePage');
   }
 
-  presentToast(err) {
-  const toast = this.toastCtrl.create({
-    message: err.message,
-    duration: 3000,
-    position: 'bottom'
-  });
+   //Move to Home Page
+   gotoHome(){
+     this.navCtrl.setRoot('HomePage');
+   }
 
-  toast.present();
-}
-presentAlert(err) {
+   presentToast(err) {
+     const toast = this.toastCtrl.create({
+       message: err.message,
+       duration: 3000,
+       position: 'bottom'
+     });
 
-}
+     toast.present();
+   }
+   presentAlert(err) {
 
-managePassword() {
-  if(this.passwordtype == 'password'){
-    this.passwordtype='text';
-    this.passeye='eye-off';
-  }else{
-    this.passwordtype='password';
-    this.passeye = 'eye';
-  }
-}
-forgetpassword(){
-  this.navCtrl.setRoot('ForgetPage');
-}
+   }
 
-}
+   managePassword() {
+     if(this.passwordtype == 'password'){
+       this.passwordtype='text';
+       this.passeye='eye-off';
+     }else{
+       this.passwordtype='password';
+       this.passeye = 'eye';
+     }
+   }
+   forgetpassword(){
+     this.navCtrl.setRoot('ForgetPage');
+   }
+
+ }
