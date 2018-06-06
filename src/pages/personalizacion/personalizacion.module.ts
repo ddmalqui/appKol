a01ec53f70d3039dd1c2634430c92c8692279f8c
ddmalqui <ddmalqui@gmail.com>
2018-06-06 06:50:59 -0300

    algo

--- src/pages/personalizacion/personalizacion.module.ts
+++ src/pages/personalizacion/personalizacion.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { PersonalizacionPage } from './personalizacion';
 
+import { GoogleCardLayout3Module } from '../../components/list-view/google-card/layout-3/google-card-layout-3.module';
+
+
 @NgModule({
   declarations: [
     PersonalizacionPage,
   ],
   imports: [
-    IonicPageModule.forChild(PersonalizacionPage),
+    IonicPageModule.forChild(PersonalizacionPage),GoogleCardLayout3Module
   ],
 })
 export class PersonalizacionPageModule {}
--- src/pages/personalizacion/personalizacion.ts
+++ src/pages/personalizacion/personalizacion.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ListViewGoogleCardsService } from '../../services/list-view-google-card-service';
+
 
 /**
  * Generated class for the PersonalizacionPage page.
@@ -12,10 +14,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-personalizacion',
   templateUrl: 'personalizacion.html',
+   providers: [ListViewGoogleCardsService]
 })
 export class PersonalizacionPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+  	public googleCardSrv: ListViewGoogleCardsService) {
+
+  	this.page =  {
+                      "title": "PRODUCTOS",
+                      "theme": "Layout3"
+                    }
+       	this.params = googleCardSrv.prepareParams(this.page); 
+       	this.params.data = googleCardSrv.load(this.page);
   }
 
   ionViewDidLoad() {
